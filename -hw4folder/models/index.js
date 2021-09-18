73f66d04356c8fe506d348b813e0882ae654bfbf
taras-vlas <68924583+taras-vlas@users.noreply.github.com>
2021-09-18 16:13:58 +0300

    hw04 -

--- -hw4folder/models/index.js
+++
@@ -1,9 +0,0 @@
-// const { Contact } = require('./contact')
-const { User } = require('./user')
-const { Order } = require('./order')
-
-module.exports = {
-  // Contact,
-  User,
-  Order,
-}
--- -hw4folder/routes/api/index.js
+++
@@ -1,9 +0,0 @@
-// const contacts = require('./contacts')
-const usersRouter = require('./users')
-const ordersRouter = require('./orders')
-
-module.exports = {
-//  contacts,
-  usersRouter,
-  ordersRouter,
-}
